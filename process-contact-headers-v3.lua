ad150ba0b1cd88708f13e75241122fb64139fdc4
jonathanelscpt <jonathanelscpt@gmail.com>
2016-09-18 11:11:41 +0200

    added scripts

---
+++ process-contact-headers-v3.lua
@@ -0,0 +1,64 @@
+--[[
+
+	Author : Jonathan Els
+	Email  : jonathanelscpt@gmail.com
+	Version : 3.0
+
+	Description:
+	
+		Find and replace FQDN with IP in Contact headers.
+		Script is applied to Inbound INVITEs and all Inbound INVITE responses
+	
+	Future development:
+	
+		None planned
+
+--]]
+
+M = {}
+
+trace.enable()
+
+-- Customer-specific DNS table initialization - edit on per-script basis
+local dns = {}
+dns["1.1.1.1"] = "hostname1%.rootdomain%.com"
+dns["2.2.2.2"] = "hostname2%.rootdomain%.com"
+dns["3.3.3.3"] = "hostname3%.subdomain%.rootdomain%.com"
+
+
+local function process_contact_headers
+
+	-- Get Contact header
+    local contact = msg:getHeader("Contact")
+	local iptest = "@(%d+%.%d+%.%d+%.%d+)"
+	
+	-- Check if exists and if URI host portion is FQDN
+	if contact and not contact:match(iptest) then
+		trace.format(" -- Contact URI host portion matched FQDN")
+		trace.format(" -- Contact header is : %s", contact)
+		
+		-- Iterate over domain and substitute if matched
+		for ip,fqdn in pairs(dns) do
+			if contact:match(fqdn) then
+				contact = contact:gsub(fqdn, ip)
+				trace.format(" -- Matched on : %s", fqdn)
+				trace.format(" -- Modified to : %s", ip)
+
+				-- Modify contact header
+				msg:modifyHeader("Contact", contact)
+				break
+			end
+		end
+	end
+
+
+end
+
+-- Apply to INVITE request and all INVITE responses
+M.inbound_INVITE = process_contact_headers
+M.inbound_ANY_INVITE = process_contact_headers
+
+
+
+
+return M
---
+++ public_connection_ip.lua
@@ -0,0 +1,47 @@
+M = {}
+
+-- enable tracing for writing to SDL
+trace.enable()
+
+local function modify_c_line_ip(msg)
+
+    -- get public ip from script param and fetch sdp
+    local public_ip = scriptParameters.getValue("public_ip")
+    local sdp = msg:getSdp() 
+
+    if public_ip and sdp
+    then
+        trace.format("Public IP script param set to %s", public_ip)
+        trace.format("SDP found... proceeding with script")
+        -- get connection line from sdp by matching only on start-of-line
+        local c_line = sdp:getLine("c=", nil)
+        
+        if c_line
+        then
+            trace.format("Connection line (c) is: %s", c_line)
+            -- match existing ip address in c line.
+            -- Simple numeric string match for IP - not robust
+            local private_ip = c_line:match("%d+%.%d+%.%d+%.%d+")
+            trace.format("Matched existing Private IP is: %s", private_ip)
+            -- substitue ip addresses in connection line
+            new_c_line = c_line:gsub(private_ip, public_ip)
+            trace.format("c line updated to: %s", new_c_line)
+            -- update sdp with new connection line
+            sdp = sdp:modifyLine("c=", nil, new_c_line)
+            -- set sdp to updated sdp
+            msg:setSdp(sdp)
+        end
+    end
+end
+
+-- apply modification to affected
+-- call flow scenarios
+
+-- INVITE and 200 OK should be affected, and needs to support EO and DO call flows
+M.inbound_INVITE = modify_c_line_ip -- EO inbound request (INVITE) from phone
+M.inbound_ANY_INVITE = modify_c_line_ip -- EO/DO inbound request and reponse (200 OK and ACK) from phone
+
+-- we should also cater for early media
+M.inbound_18X = modify_c_line_ip -- support early media in inbound 180 and 183 messages
+
+return M
