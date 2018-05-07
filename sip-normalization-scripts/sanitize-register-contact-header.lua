4b90e0b0a862eb06f34a70857ade620315ec7536
Jonathan Els <jonathan.els@dimensiondata.com>
2018-05-07 19:07:20 +0200

    new script added from req on cisco communities

---
+++ sip-normalization-scripts/sanitize-register-contact-header.lua
@@ -0,0 +1,34 @@
+--[[
+    Author : Jonathan Els
+    Version : 0.1
+    
+    Description:
+    
+        Sanitize contact heard to strip attributes and prefix a "q value" for inbound REGISTER messages.  A "q = 1" suffix is hard-coded.
+
+        Converts this:
+
+            Contact: <sip: 3001@10.0.1.56: 50772>; + sip.instance = "<urn: uuid: 832028f41e6258c46e8de1e8dd3d6723360b5c74>"; reg-id = 1
+
+        To this:
+
+            Contact: <sip: 3001@10.0.1.56: 50772>; q = 1
+--]]
+
+
+M = {}
+
+trace.disable()
+
+function M.inbound_REGISTER(msg)
+    local contact = msg:getHeader("Contact")
+    if contact
+    then
+        local qval = "q = 1"
+        contact = contact:gsub("(.*<sip:.*>);.*", "%1" .. "; " .. qval)
+        trace.format("Found contact header... modifying to: %s", contact)
+        msg:modifyHeader ("Contact", contact)
+    end
+end
+
+return M
